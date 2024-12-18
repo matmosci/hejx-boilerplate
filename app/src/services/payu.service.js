5939b7ba4da4b7a35742f1b1a7c2ae57e9825267
matmosci <matmosci@gmail.com>
2024-12-19 00:16:59 +0100

    implement PayU OAuth token handling and request function

--- app/src/services/payu.service.js
+++ app/src/services/payu.service.js
@@ -1,5 +1,39 @@
 const crypto = require('crypto');
 
+let oAuthToken = null;
+
+async function request(config) {
+    const response = await fetch(config.url, {
+        method: config.method,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${await getOAuthToken()}`
+        },
+        body: JSON.stringify(config.body)
+    });
+    if (response.status === 401) {
+        await getOAuthToken(true);
+        return request(config);
+    };
+    return response.json();
+};
+
+async function getOAuthToken(force = false) {
+    if (!oAuthToken || force) oAuthToken = (await generateOAuthToken()).access_token;
+    return oAuthToken;
+};
+
+async function generateOAuthToken() {
+    const response = await fetch(`${global.config.PAYU.api_url}/pl/standard/user/oauth/authorize`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: `grant_type=client_credentials&client_id=${global.config.PAYU.client_id}&client_secret=${global.config.PAYU.client_secret}`
+    });
+    return response.json();
+};
+
 function compare(object) {
     const incoming_signature = signatureHeaderToObject(object.signature_header).signature;
     const expected_signature = crypto.createHash('md5').update(object.json + object.second_key).digest('hex');
@@ -14,16 +48,4 @@ function signatureHeaderToObject(signatureHeader) {
     }, {});
 };
 
-async function generateOAuthToken() {
-    const response = await fetch(`${global.config.PAYU.api_url}/pl/standard/user/oauth/authorize`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: `grant_type=client_credentials&client_id=${global.config.PAYU.client_id}&client_secret=${global.config.PAYU.client_secret}`
-    });
-    const token = await response.json();
-    return token;
-};
-
-module.exports = { compare, generateOAuthToken };
+module.exports = { request, compare };

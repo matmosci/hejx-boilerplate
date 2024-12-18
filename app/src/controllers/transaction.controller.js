dba66259c7b36f446eb67926025ac63b35500679
matmosci <matmosci@gmail.com>
2024-12-18 02:22:57 +0100

    add PayU transaction controller and service

---
+++ app/src/controllers/transaction.controller.js
@@ -0,0 +1,22 @@
+const { send } = require("../utils/mailer.utils");
+const { compare } = require("../services/payu.service");
+
+module.exports = {
+    onNotification: (req, res) => {
+        const signature_header = req.get("OpenPayu-Signature");
+        const json = JSON.stringify(req.body);
+        const second_key = global.config.payu.second_key;
+
+        const { order } = req.body;
+        const { orderId, status } = order;
+
+        if (!compare({ signature_header, json, second_key })) {
+            send(`PayU transaction not verified`, `Transaction ${orderId} is NOT VERIFIED<br>${order}`, global.config.CONTACTS.admin.email);
+            return res.status(400);
+        }
+
+        res.end();
+        if (!["COMPLETED", "CANCELED"].includes(status)) return;
+        send(`PayU ${status} transaction`, `Transaction ${orderId} has been ${status}`, global.config.CONTACTS.admin.email);
+    },
+};
--- app/src/routers/transaction.router.js
+++ app/src/routers/transaction.router.js
@@ -1,25 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { send } = require('../utils/mailer.utils');
-const { compare } = require('../utils/payu.utils');
+const transaction = require('../controllers/transaction.controller');
 
-router.post('/notify', (req, res) => {
-    const signature_header = req.get("OpenPayu-Signature");
-    const json = JSON.stringify(req.body);
-    const second_key = global.config.payu.second_key;
-
-    const { order } = req.body;
-    const { orderId, status } = order;
-
-    if (!compare({ signature_header, json, second_key })) {
-        send(`PayU transaction not verified`, `Transaction ${orderId} is NOT VERIFIED<br>${order}`, global.config.CONTACTS.admin.email);
-        return res.status(400);
-    }
-
-    res.end();
-    if (!["COMPLETED", "CANCELED"].includes(status)) return;
-    send(`PayU ${status} transaction`, `Transaction ${orderId} has been ${status}`, global.config.CONTACTS.admin.email);
-});
+router.post('/notify', transaction.onNotification);
 router.get('/status', (req, res) => {
     if (req.query.error) return res.send(`Payment failed. <a href='${global.config.BASE_URL}'>[Back]</a>`);
     res.send(`Thank you for your payment. <a href='${global.config.BASE_URL}'>[Back]</a>`);

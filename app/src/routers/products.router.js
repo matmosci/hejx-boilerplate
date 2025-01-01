44c45d69109fe26ff9e47fc97d7c7d6d34a03084
matmosci <matmosci@gmail.com>
2025-01-01 22:26:09 +0100

    fix product router by swap handlers

--- app/src/routers/products.router.js
+++ app/src/routers/products.router.js
@@ -4,7 +4,7 @@ const controller = require('../controllers/products.controller');
 
 router.get('/', controller.getContainerMain);
 router.get('/category/:category', controller.getContainer);
-router.get('/:product', controller.getProduct);
-router.get('/:product/*', controller.getProductDefault);
+router.get('/:product', controller.getProductDefault);
+router.get('/:product/*', controller.getProduct);
 
 module.exports = router;

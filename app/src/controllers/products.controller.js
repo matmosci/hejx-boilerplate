5c8b3fef0897cb48d98fd35f9d5d2cf26e8a60fb
matmosci <matmosci@gmail.com>
2025-01-06 18:52:02 +0100

    refactor product controller

--- app/src/controllers/products.controller.js
+++ app/src/controllers/products.controller.js
@@ -1,3 +1,5 @@
+const { keyValueArraysToObject } = require("../utils/common.utils");
+const render = require("../utils/render.utils");
 const {
     getProductConfigured,
     getProductParamNames,
@@ -6,47 +8,49 @@ const {
     getContainerGridItems,
     parseGridItem
 } = require("../services/products.service");
-const { keyValueArraysToObject } = require("../utils/common.utils");
-const render = require("../utils/render.utils");
 
 module.exports = {
-    getContainer: (req, res) => {
-        const { category: categoryName } = req.params;
-        const category = parseGridItem(categoryName);
-        render(req, res, "products", { items: getContainerGridItems(category.name), title: category.title });
-    },
-    getContainerMain: (req, res) => {
-        render(req, res, "products", { items: getContainerGridItems("home"), title: "Products" });
-    },
-    getProductWithConfig: (req, res) => {
-        const { product: productName, 0: configURN } = req.params;
-        const configArr = configURN.split("/");
-        try {
-            const config = keyValueArraysToObject(getProductParamNames(productName), configArr);
-            console.log(config); // dev
-            const { product, reConfigArr } = getProductConfigured(productName, config);
-            const url = `/products/${product.name}/${reConfigArr.join("/")}`;
-            res.set("HX-Push-Url", url);
-            render(req, res, "product", { product, title: product.title });
-        } catch (error) {
-            console.log(error); // dev
-            getProductDefault(req, res);
-        }
-    },
-    getProductDefault
+    getContainerMain,
+    getContainer,
+    getProductDefault,
+    getProductWithConfig
 };
 
 function getProductDefault(req, res) {
+    const { product: productName } = req.params;
     try {
-        const { product: productName } = req.params;
         const product = getProductDefinition(productName);
         const paramURN = getProductDefaultParamValues(product).map(p => p.value).join("/");
         const url = `/products/${product.name}/${paramURN}`;
         res.set("HX-Push-Url", url);
         render(req, res, "product", { product, title: product.title });
     } catch (error) {
-        const url = `/products`;
+        getContainerMain(req, res);
+    }
+};
+
+function getProductWithConfig(req, res) {
+    const { product: productName, 0: configURN } = req.params;
+    const configArr = configURN.split("/");
+    try {
+        const config = keyValueArraysToObject(getProductParamNames(productName), configArr);
+        console.log(config); // dev
+        const { product, reConfigArr } = getProductConfigured(productName, config);
+        const url = `/products/${product.name}/${reConfigArr.join("/")}`;
         res.set("HX-Push-Url", url);
         render(req, res, "product", { product, title: product.title });
+    } catch (error) {
+        console.log(error); // dev
+        getProductDefault(req, res);
     }
+};
+
+function getContainer(req, res) {
+    const { category: categoryName } = req.params;
+    const category = parseGridItem(categoryName);
+    render(req, res, "products", { items: getContainerGridItems(category.name), title: category.title });
+};
+
+function getContainerMain(req, res) {
+    render(req, res, "products", { items: getContainerGridItems("home"), title: "Products" });
 };

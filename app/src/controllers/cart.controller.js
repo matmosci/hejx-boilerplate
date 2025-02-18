972c5521417c17826a5dceabbbb5f939e6aa337d
matmosci <matmosci@gmail.com>
2025-02-18 21:54:17 +0100

    refactor shipping methods: rename routes, update model, and replace shipment component with shipping component

--- app/src/controllers/cart.controller.js
+++ app/src/controllers/cart.controller.js
@@ -1,6 +1,6 @@
 const service = require('../services/cart.service');
 const render = require("../utils/render.utils");
-const shipmentMethods = require("../../data/shipmentMethods"); // TODO to service
+const shippingMethods = require("../../data/shippingMethods"); // TODO to service
 
 module.exports = {
     getCart,
@@ -11,7 +11,8 @@ module.exports = {
     updateProduct,
     removeProduct,
     checkout,
-    getShipment,
+    getShipping,
+    updateShipping,
 };
 
 async function getCart(req, res) {
@@ -61,6 +62,10 @@ async function addProduct(req, res) {
     }
 };
 
+function updateProduct(req, res) {
+    res.send('updateProduct');
+};
+
 async function removeProduct(req, res) {
     const productId = req.params.id;
     if (!productId) res.sendStatus(400);
@@ -76,19 +81,45 @@ async function removeProduct(req, res) {
 async function checkout(req, res) {
     try {
         const cart = await service.getUserCart(req.session.user._id);
-        render(req, res, "checkout", { cart, shipmentMethods });
+        render(req, res, "checkout", { cart, shippingMethods });
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
     }
 };
 
-async function getShipment(req, res) {
-    const selectedShipmentMethod = req.query['selected-shipment-method'];
-    if (!selectedShipmentMethod) return res.end();
-    res.send(`[${selectedShipmentMethod} details]`);
+async function getShipping(req, res) {
+    const cart = await service.getUserCart(req.session.user._id);
+    const selectedShippingMethod = req.query['selected-shipping-method'];
+    if (!selectedShippingMethod) return res.end();
+    const method = shippingMethods.find(method => method.name === selectedShippingMethod);
+    res.render('components/cartCheckoutShippingMethod', { cart, method, user: req.session.user });
 };
 
-function updateProduct(req, res) {
-    res.send('updateProduct');
+async function updateShipping(req, res) {
+    const cart = await service.getUserCart(req.session.user._id);
+    cart.shipping.selectedMethod = req.body['selected-shipping-method'];
+    const details = { ...req.body };
+    delete details['selected-shipping-method'];
+    cart.shipping.details.set(req.body['selected-shipping-method'], details);
+    await cart.save();
+    res.render('components/cartCheckoutShippingSelected', { cart, shippingMethods });
 };
+
+(async () => {
+    const Cart = require("../models/Cart.model");
+    // const cart = await Cart.findOne({userId:"6784ec390dc80aa431160cf9"});
+    // const cart = await Cart.create({userId:"6784ec390dc80aa431160cf9"});
+    // console.log(cart);
+    // await Cart.findByIdAndDelete(cart._id);
+    // await Cart.findOneAndDelete({ userId: "6784ec390dc80aa431160cf9" });
+
+    // cart.shipping.selectedMethod = "curier";
+    // cart.shipping.details.set('curier', {town: "asd", street: "asd", number: "asd", postalCode: "asd"});
+    // await cart.save();
+    // console.log(cart.shipping);
+
+    // console.log(await Cart.find({ userId: "6784ec390dc80aa431160cf9" }));
+    // console.log((await Cart.findOne({userId:"6784ec390dc80aa431160cf9"})).shipping);
+
+})();
--- app/src/models/Cart.model.js
+++ app/src/models/Cart.model.js
@@ -1,5 +1,13 @@
 const mongoose = require("mongoose");
 
+const shippingSchema = new mongoose.Schema(
+    {
+        selectedMethod: { type: String },
+        details: { type: Map, of: Object },
+    },
+    { _id: false, minimize: false }
+);
+
 const cartProductSchema = new mongoose.Schema(
     {
         id: { type: String, required: true },
@@ -16,9 +24,12 @@ const schema = new mongoose.Schema(
     {
         userId: { type: mongoose.Types.ObjectId, required: true },
         content: { type: [cartProductSchema], default: [] },
+        shipping: { type: shippingSchema, default: { selectedMethod: "", details: new Map() } },
         costSubtotal: { type: Number, default: 0 },
+        costShipping: { type: Number, default: 0 },
+        costTotal: { type: Number, default: 0 },
     },
     { timestamps: true }
-)
+);
 
 module.exports = mongoose.model("Cart", schema);
